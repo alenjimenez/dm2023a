ac5212718f8cc80e4e260ee60abd6d88b1f0e610
alenjimenez <jimenezalen@gmail.com>
2023-06-03 19:19:20 -0300

    FE_segunda

--- src/workflow-semillerio/731_FE_historia_0.r
+++ src/workflow-semillerio/731_FE_historia_0.r
@@ -407,6 +407,17 @@ if( PARAM$lag1 )
   GrabarOutput()
 }
 
+if( PARAM$CanaritosAsesinos$ratio > 0.0)
+{
+  OUTPUT$CanaritosAsesinos$ncol_antes  <- ncol(dataset)
+  CanaritosAsesinos( canaritos_ratio= PARAM$CanaritosAsesinos$ratio,
+                     canaritos_desvios= PARAM$CanaritosAsesinos$desvios,
+                     canaritos_semilla=  PARAM$CanaritosAsesinos$semilla )
+  
+  OUTPUT$CanaritosAsesinos$ncol_despues  <- ncol(dataset)
+  GrabarOutput()
+}
+
 if( PARAM$lag2 )
 {
   #creo los campos lags de orden 2
@@ -422,6 +433,17 @@ if( PARAM$lag2 )
   }
 
   OUTPUT$lag2$ncol_despues  <- ncol(dataset)
+  GrabarOutput()
+}
+
+if( PARAM$CanaritosAsesinos$ratio > 0.0)
+{
+  OUTPUT$CanaritosAsesinos$ncol_antes  <- ncol(dataset)
+  CanaritosAsesinos( canaritos_ratio= PARAM$CanaritosAsesinos$ratio,
+                     canaritos_desvios= PARAM$CanaritosAsesinos$desvios,
+                     canaritos_semilla=  PARAM$CanaritosAsesinos$semilla )
+  
+  OUTPUT$CanaritosAsesinos$ncol_despues  <- ncol(dataset)
   GrabarOutput()
 }
 
@@ -475,6 +497,17 @@ if( PARAM$Tendencias1$run )
                       ratiomax=  PARAM$Tendencias1$ratiomax  )
 
   OUTPUT$TendenciasYmuchomas1$ncol_despues  <- ncol(dataset)
+  GrabarOutput()
+}
+
+if( PARAM$CanaritosAsesinos$ratio > 0.0)
+{
+  OUTPUT$CanaritosAsesinos$ncol_antes  <- ncol(dataset)
+  CanaritosAsesinos( canaritos_ratio= PARAM$CanaritosAsesinos$ratio,
+                     canaritos_desvios= PARAM$CanaritosAsesinos$desvios,
+                     canaritos_semilla=  PARAM$CanaritosAsesinos$semilla )
+  
+  OUTPUT$CanaritosAsesinos$ncol_despues  <- ncol(dataset)
   GrabarOutput()
 }
 
